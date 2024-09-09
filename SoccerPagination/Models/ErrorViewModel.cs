9ed72c19817fa73475e5df52d0450839366ba9cd
Petr Zalizko <pzalizko@gmail.com>
2024-09-09 21:55:46 +0300

    Add project files.

---
+++ SoccerPagination/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoccerPagination.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
